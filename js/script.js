8c765f7d230804ced787b4e6b3e7946ca5b9ade1
Waliston Euripedes <goneisgone@hotmail.com>
2022-12-05 14:57:55 -0300

    menu lateral end

--- js/script.js
+++ js/script.js
@@ -5,6 +5,24 @@ let canvaBar = document.querySelector(".canva-toggler")
 
 function showmenu() {
     canvaBar.classList.toggle("canva-toggler");
+    
+    setTimeout( () => {
+        canvaBar.classList.add("canva-toggler");
+    }, 3000)
 }
 
 btnCanvaControl.addEventListener("click", showmenu);
+
+/*
+const btn = document.getElementById('elem');
+
+btn.addEventListener('click', () => window.scrollTo({
+  top: 400,
+  behavior: 'smooth',
+}));
+
+#x {
+  height: 1000px;
+  background: lightblue;
+}
+*/
