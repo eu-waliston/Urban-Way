0db6190154b719b66bba37ad2218b3b26c2b2102
Waliston Euripedes <goneisgone@hotmail.com>
2022-12-05 13:02:23 -0300

    menu lateral almost end

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
-
 let btnCanvaControl = document.querySelector("#btn-canva-control")
 
 let canvaBar = document.querySelector(".canva-toggler")
 
+
 function showmenu() {
     canvaBar.classList.toggle("canva-toggler");
+    canvaBar.animate()
 }
 
 btnCanvaControl.addEventListener("click", showmenu);
