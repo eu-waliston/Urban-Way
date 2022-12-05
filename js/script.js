e2984ca3b229b3061fae2e0093cbe89e1f6b8802
Waliston Euripedes <goneisgone@hotmail.com>
2022-12-05 12:14:03 -0300

    menu lateral

--- js/script.js
+++ js/script.js
@@ -1,17 +1,12 @@
-import anime from '/animejs/lib/anime.es.js';
+
 
 let btnCanvaControl = document.querySelector("#btn-canva-control")
 
 let canvaBar = document.querySelector(".canva-toggler")
-
-
-anime({
-    targets: '.canva-toggler',
-    translateX: 250
-});
 
 function showmenu() {
     canvaBar.classList.toggle("canva-toggler");
 }
 
 btnCanvaControl.addEventListener("click", showmenu);
+
