daa7d52b9396f3a529b2ffe8e2bacd19d24d3837
Waliston Euripedes <goneisgone@hotmail.com>
2022-12-05 11:26:04 -0300

    menu latera

---
+++ js/script.js
@@ -0,0 +1,13 @@
+
+
+let btnCanvaControl = document.querySelector("#btn-canva-control")
+
+let canvaBar = document.querySelector(".canva-toggler")
+
+function showmenu() {
+    canvaBar.classList.toggle("canva-toggler");
+}
+
+
+
+btnCanvaControl.addEventListener("click", showmenu);
