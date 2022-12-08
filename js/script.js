89c039d05ac6f8fd5f164f7a11480e5db4d89e0f
Waliston Euripedes <goneisgone@hotmail.com>
2022-12-08 16:06:50 -0300

    Urban Way

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+//import anime from '../js/anime.es';
+
 
 let btnCanvaControl = document.querySelector("#btn-canva-control")
 
@@ -5,12 +7,12 @@ let canvaBar = document.querySelector(".canva-toggler")
 
 
 function showmenu() {
-  canvaBar.classList.toggle("canva-toggler");
+    canvaBar.classList.toggle("canva-toggler");
 
-  setTimeout(() => {
-    canvaBar.classList.add("canva-toggler");
+    setTimeout(() => {
+        canvaBar.classList.add("canva-toggler");
 
-  }, 3000)
+    }, 3000)
 }
 
 btnCanvaControl.addEventListener("click", showmenu);
