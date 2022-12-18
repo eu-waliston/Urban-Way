98fb952792efc66b3c37eb220beccb025ef92bcd
Waliston Euripedes <goneisgone@hotmail.com>
2022-12-18 11:06:45 -0300

    Urban Way

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require("express");
 const path = require("path");
 const ejs = require('ejs')
 
+// const axios = require('axios');
+
+// axios.get('https://api.unsplash.com/photos?page=2').then( (res) => {
+//     console.log(res);
+// })
+
 const app = new express();
 app.set('view engine','ejs')
 
--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,3 @@
-//import anime from '../js/anime.es';
-
-
 let btnCanvaControl = document.querySelector("#btn-canva-control")
 
 let canvaBar = document.querySelector(".canva-toggler")
@@ -17,16 +14,5 @@ function showmenu() {
 
 btnCanvaControl.addEventListener("click", showmenu);
 
-/*
-const btn = document.getElementById('elem');
 
-btn.addEventListener('click', () => window.scrollTo({
-  top: 400,
-  behavior: 'smooth',
-}));
 
-#x {
-  height: 1000px;
-  background: lightblue;
-}
-*/
