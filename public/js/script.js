a555c7c18069265a95621108e7135991a006930d
Waliston Euripedes <goneisgone@hotmail.com>
2022-12-18 11:07:18 -0300

    Urban Way

--- public/js/script.js
+++ public/js/script.js
@@ -2,17 +2,8 @@ let btnCanvaControl = document.querySelector("#btn-canva-control")
 
 let canvaBar = document.querySelector(".canva-toggler")
 
-
 function showmenu() {
-    canvaBar.classList.toggle("canva-toggler");
-
-    // setTimeout(() => {
-    //     canvaBar.classList.add("canva-toggler");
-
-    // }, 3000)
+  canvaBar.classList.toggle("canva-toggler");
 }
 
 btnCanvaControl.addEventListener("click", showmenu);
-
-
-
