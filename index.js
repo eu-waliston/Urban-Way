525c0658a35141bc75a80d15252feb5e805c8e33
walwizard <goneisgone@hotmail.com>
2023-01-11 22:36:11 -0300

    Price Section

--- index.js
+++ index.js
@@ -1,28 +1,7 @@
 const express = require("express");
-const path = require("path");
-const ejs = require('ejs')
-
-// const axios = require('axios');
-
-// axios.get('https://api.unsplash.com/photos?page=2').then( (res) => {
-//     console.log(res);
-// })
-
-// cep.addEventListener("blur", (e) => {
-//     let search = cep.value.replace("-", "")
-//     const options = {
-//         method: "GET",
-//         mode: "cors",
-//         cache: "default"
-//     }
-//     fetch(`https://viacep.com.br/ws/${search}/json/`, options).then(response => {
-//         response.json().then(data => showData(data))
-//     }).catch(e => console.log(`Deu Erro: ${e, message}`))
-// })
-
 const app = new express();
-app.set('view engine','ejs')
 
+app.set('view engine','ejs')
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
