b68b3163b1ea38941cfe784d0d3e3fa0b8c01b80
walwizard <goneisgone@hotmail.com>
2023-01-15 15:35:40 -0300

    Scroll top function

--- public/js/script.js
+++ public/js/script.js
@@ -1,9 +1,32 @@
-let btnCanvaControl = document.querySelector("#btn-canva-control")
+let btnCanvaControl = document.querySelector("#btn-canva-control");
 
-let canvaBar = document.querySelector(".canva-toggler")
+let canvaBar = document.querySelector(".canva-toggler");
 
 function showmenu() {
   canvaBar.classList.toggle("canva-toggler");
 }
 
 btnCanvaControl.addEventListener("click", showmenu);
+
+// Get the button:
+let mybutton = document.querySelector("#myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.documentElement.scrollTop = 0;
+}
+
+mybutton.addEventListener('click', topFunction)
